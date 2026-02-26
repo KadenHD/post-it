047f5d7c9b1d716e6fb74e1ceaae307bc733f7f6
KadenHD <71968563+KadenHD@users.noreply.github.com>
2026-02-26 18:54:18 +0100

    add anchor to the logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <header>
             <nav>
-                <Link href="/" className="logo">
+                <Link href="/#home" className="logo">
                     <Image src="/icons/logo.png" alt="logo" width={24} height={24} />
                     <p>Post'IT</p>
                 </Link>
